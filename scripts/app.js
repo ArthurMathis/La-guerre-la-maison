8f4af364ef1270a9d0b4511e47e64ff934748e00
ArthurMathis <mathis.arthur68@gmail.com>
2023-12-19 02:50:20 +0100

    Ajout des pop-up

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,53 @@
-const letters = document.querySelectorAll('article img');
+const letters = document.querySelectorAll('.annecdote');
 const msgBox = document.getElementById('msgBox');
+const Box = document.getElementById('Box');
 const msgText =  document.getElementById('msgtext');
+const annecdotes = document.querySelectorAll('.annotation');
+
+function addImage(img){
+    return "<img src=\"" + img + "\">";
+}
 
 function affiche(msg){
-    msgBox.style.opacity = '1';
+    msgBox.style.display = 'flex';
+    setTimeout(function() {
+        Box.style.opacity = '1';
+    }, 1);
     msgText.innerHTML = msg;
 }
 
+function afficheImage(img){
+    msgBox.style.display = 'flex';
+    setTimeout(function() {
+        Box.style.opacity = '1';
+    }, 1);
+    msgText.innerHTML = addImage(img);
+}
+
 function remove(){
-    msgBox.style.opacity = "0";
+    msgBox.style.display = 'none';
+    Box.style.opacity = "0";
     msgText.innerHTML = '';
 }
 
 function isOn(element) {
+    afficheImage('../assets/img/1.jpg');
     element.style.opacity = '0';
+    removeFantome();
+    letters.forEach(c => {
+        if(c !== element){
+            c.style.zIndex = '-10';
+        }
+    });
 }
 
 function isOf(element) {
+    remove();
+    afficheFantome();
     element.style.opacity = '1';
+    letters.forEach(c => {
+        c.style.zIndex = 'auto';
+    });
 }
 
 function init() {
@@ -25,6 +55,16 @@ function init() {
         letters[i].addEventListener('mouseenter', (event) => { isOn(event.target); });
         letters[i].addEventListener('mouseleave', (event) => { isOf(event.target); });
     }
+}
+
+function afficheFantome(){
+    annecdotes[0].style.opacity = '1';
+    annecdotes[4].style.opacity = '1';
+}
+
+function removeFantome(){
+    annecdotes[0].style.opacity = '0';
+    annecdotes[4].style.opacity = '0';
 }
 
 document.addEventListener('DOMContentLoaded', init);
