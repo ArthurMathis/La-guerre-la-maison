c3dd6e06f4eb1560853d949314aeaebbb10aaf8a
ArthurMathis <mathis.arthur68@gmail.com>
2023-12-21 01:03:55 +0100

    Version à rendre

--- scripts/view.js
+++ scripts/view.js
@@ -42,5 +42,35 @@ function hideGalerie(){
     document.getElementById('galerie').style.backdropFilter = 'none';
     document.getElementById('menu-icon').style.display = 'none';
 }
-  
-window.addEventListener('load', apparitionProgressiveImages);
+
+const images = document.querySelectorAll('#galerie img');
+
+// Options pour l'IntersectionObserver
+const options = {
+    root: null, // La racine à partir de laquelle observer (la fenêtre par défaut)
+    rootMargin: '0px', // Marge autour de la racine
+    threshold: 0.2 // Pourcentage de l'élément visible pour déclencher l'intersection
+};
+
+// Créer une instance de l'IntersectionObserver
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        // Si l'élément est partiellement ou totalement visible
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1; // Mettre l'opacité à 1
+            console.log(`L'élément : ${entry} est visible.`)
+        } else {
+            entry.target.style.opacity = 0; // Mettre l'opacité à 0
+        }
+    });
+}, options);
+
+function init(){
+    images.forEach(image => {
+        observer.observe(image);
+        console.log(image);
+    });
+    apparitionProgressiveImages();
+}
+
+window.addEventListener('load', init);
